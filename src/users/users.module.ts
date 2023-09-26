e4f7640009de578655a7edc1226b84f86e69e2f0
OEH808 <omarehababdelaziz8@gmail.com>
2023-09-26 15:02:19 +0300

    Created AuthService

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,10 +3,11 @@ import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './user.schema';
+import { AuthService } from './auth.service';
 
 @Module({
   imports: [MongooseModule.forFeature([{name: User.name, schema: UserSchema}])],
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [UsersService, AuthService]
 })
 export class UsersModule {}
