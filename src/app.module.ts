5481d3c24afdb4caf972a64ba65b4bf09d62f34d
OEH808 <omarehababdelaziz8@gmail.com>
2023-09-26 13:01:26 +0300

    Added mongo database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,27 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ItemsModule } from './items/items.module';
 import { CategoriesModule } from './categories/categories.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService} from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule, ItemsModule, CategoriesModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.mongo`
+    }),
+    UsersModule, 
+    CategoriesModule, 
+    ItemsModule, 
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],  
+      useFactory: (config: ConfigService) => {
+        return {
+          uri: config.get<string>('DB_URI'),
+        }
+      }
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
