756ff8a6148511a225bb988c2ca6fb670af34fc7
OEH808 <omarehababdelaziz8@gmail.com>
2023-09-26 14:43:57 +0300

    Added dto for user creation

---
+++ src/users/create-user.dto.ts
@@ -0,0 +1,12 @@
+import { IsEmail, IsString, IsOptional } from "class-validator";
+
+export class CreateUserDto {
+    @IsString()
+    name: string;
+    
+    @IsEmail()
+    email: string;
+
+    @IsString()
+    password: string;
+}
