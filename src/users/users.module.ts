fd5e8bd7af8dea7c2f33eb764705d1d5c1536dcf
OEH808 <omarehababdelaziz8@gmail.com>
2023-09-27 13:05:33 +0300

    Added authorization module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -4,9 +4,10 @@ import { UsersService } from './users.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './user.schema';
 import { AuthService } from './auth.service';
+import { AuthModule } from './auth.module';
 
 @Module({
-  imports: [MongooseModule.forFeature([{name: User.name, schema: UserSchema}])],
+  imports: [MongooseModule.forFeature([{name: User.name, schema: UserSchema}]), AuthModule],
   controllers: [UsersController],
   providers: [UsersService, AuthService]
 })
