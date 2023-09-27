b14872a5cafe9c981e524fa0a3bf89fa93066760
OEH808 <omarehababdelaziz8@gmail.com>
2023-09-27 14:09:56 +0300

    Created a schema for categories

--- src/categories/categories.module.ts
+++ src/categories/categories.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CategoriesController } from './categories.controller';
 import { CategoriesService } from './categories.service';
+import { Category, categorySchema } from './categories.schema';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
+  imports: [MongooseModule.forFeature([{name: Category.name, schema: categorySchema}]),],
   controllers: [CategoriesController],
   providers: [CategoriesService]
 })
---
+++ src/categories/categories.schema.ts
@@ -0,0 +1,18 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+@Schema()
+export class Category {
+    @Prop({ required: [true, "Category must have a name."], trim: true })
+    name: string;
+    
+    @Prop({ required: [true, "Category must have an image"], trim: true  })
+    image: string;
+
+    @Prop({ required: [true, "Category must have a description."], trim: true })
+    description: string;
+}
+
+export type categoryDocument = HydratedDocument<Category>;
+
+export const categorySchema = SchemaFactory.createForClass(Category);
