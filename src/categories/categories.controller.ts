d1bb68abad205af9d1b736b70f88ab5647001200
OEH808 <omarehababdelaziz8@gmail.com>
2023-09-27 14:51:46 +0300

    Created skeleton for category CRUD

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -1,4 +1,36 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Session, Get, Patch, Delete, Param, Query } from '@nestjs/common';
+import { CategoriesService } from './categories.service';
+import { CreateCategoryDto } from './dtos/create-category.dto';
 
 @Controller('categories')
-export class CategoriesController {}
+export class CategoriesController {
+    constructor(private categoryService: CategoriesService) {}
+
+    @Post()
+    async createCategory(@Body() body: CreateCategoryDto) {
+        const category = await this.categoryService.create(body.name, body.image, body.description);
+
+        return category;
+    }
+
+    @Get('/:name')
+    getCategory() {
+
+    }
+
+    @Get()
+    getAllCategories() {
+
+    }
+
+    @Patch('/:name')
+    editCategory() {
+
+    }
+
+    @Delete('/:name')
+    removeCategory() {
+
+    }
+
+}
--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,4 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectConnection, InjectModel } from '@nestjs/mongoose';
+import { Category } from './categories.schema';
+import { Model } from 'mongoose';
+
 
 @Injectable()
-export class CategoriesService {}
+export class CategoriesService {
+    constructor(@InjectModel(Category.name) private categoryModel: Model<Category>) {}
+
+    // --- CREATE ---
+    create(name: string, image: string, description: string) {
+        const category = new this.categoryModel({name,image,description});
+
+        return category.save();
+    }
+
+    // --- GET ---
+    findOne() {
+
+    }
+
+    find() {
+
+    }
+
+    // --- UPDATE ---
+    edit() {
+
+    }
+
+    // --- DELETE ---
+    remove() {
+        
+    }
+
+}
