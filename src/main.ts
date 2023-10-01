d6b784354eec92aca6bcc5509695983992287507
OEH808 <omarehababdelaziz8@gmail.com>
2023-10-01 14:25:19 +0300

    Added mongodb sanitiser

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import ExpressMongoSanitize = require('express-mongo-sanitize');
 
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(ExpressMongoSanitize());
   await app.listen(3000);
   // Makes a console log of the url being used by the nest app
   //console.log(`Application is running on: ${await app.getUrl()}`);
