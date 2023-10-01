e9ae1ebc46f0ef7448b633e9adb86516c20294aa
OEH808 <omarehababdelaziz8@gmail.com>
2023-10-01 12:03:43 +0300

    Imported Jwt to module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -5,9 +5,20 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { User, userSchema } from './user.schema';
 import { AuthService } from './auth.service';
 import { AuthModule } from './auth.module';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [MongooseModule.forFeature([{name: User.name, schema: userSchema}]), AuthModule],
+  imports: [MongooseModule.forFeature([{name: User.name, schema: userSchema}]), AuthModule, 
+  JwtModule.registerAsync({
+    global: true,
+    imports: [ConfigModule],
+    useFactory: async (configService: ConfigService) => ({
+      secret: configService.get<string>('SECRET'),
+      signOptions: { expiresIn: '1d' },
+    }),
+    inject: [ConfigService],
+  }),],
   controllers: [UsersController],
   providers: [UsersService, AuthService]
 })
