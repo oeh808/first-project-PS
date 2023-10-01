9e85df4cff677a644396b3b1f1e7a11be4566aa4
OEH808 <omarehababdelaziz8@gmail.com>
2023-10-01 12:02:33 +0300

    Commented out currently unecessary code

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
   // Makes a console log of the url being used by the nest app
-  console.log(`Application is running on: ${await app.getUrl()}`);
+  //console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
