0720e44874a86e403d6178d4b7e9e4926fe20eb9
OEH808 <omarehababdelaziz8@gmail.com>
2023-10-04 10:48:25 +0300

    Set up cookie-parser in main

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import ExpressMongoSanitize = require('express-mongo-sanitize');
+import * as cookieParser from 'cookie-parser';
 
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(ExpressMongoSanitize());
+  app.use(cookieParser());
   await app.listen(3000);
   // Makes a console log of the url being used by the nest app
   //console.log(`Application is running on: ${await app.getUrl()}`);
